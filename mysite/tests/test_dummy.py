fc431c0595ce951c81bc097dc55cea5b6249c4ef
LAPTOP-UM975EAE\Qi <tjq888@gmail.com>
2022-03-04 01:35:34 +0800

    add dummy tests

--- mysite/tests/test_dummy.py
+++ mysite/tests/test_dummy.py
@@ -1,3 +1,10 @@
+from django.urls import reverse
+
 def test_nothing():
     """A dummy test"""
     assert True
+
+# def test_polls(client):
+#     url = reverse("polls")
+#     response = client.get(url)
+#     assert response.status_code == 200
