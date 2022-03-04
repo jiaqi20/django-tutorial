58b01cf17322372f6495df00340e4ec439501041
LAPTOP-UM975EAE\Qi <tjq888@gmail.com>
2022-03-04 12:25:37 +0800

    add comments

--- mysite/polls/tests/test_views.py
+++ mysite/polls/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from django.test import Client
+#from django.test import Client
 
 def test_nothing():
     """A dummy test"""
@@ -11,6 +11,7 @@ def test_index_view(client):
     url = reverse("index")
     # Make a GET request to the view using the test client
     response = client.get(url)
-    # Verify that the response is correct
+    # Verify that the response code is correct
     assert response.status_code == 200
+    # Verify that the response body is correct
     assert response.content == b"Hello, world. You're at the polls index."
