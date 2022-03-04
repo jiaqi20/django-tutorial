b2e07ada2da4c792ebda39eb0f403059a92f022e
LAPTOP-UM975EAE\Qi <tjq888@gmail.com>
2022-03-04 08:34:37 +0800

    add testcases to test index of poll

---
+++ mysite/polls/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+from django.test import Client
+
+def test_nothing():
+    """A dummy test"""
+    assert True
+
+def test_index_view(client):
+    """Test that polls view works"""
+    # Build the URL from the url's name
+    url = reverse("index")
+    # Make a GET request to the view using the test client
+    response = client.get(url)
+    # Verify that the response is correct
+    assert response.status_code == 200
+    assert response.content == b"Hello, world. You're at the polls index."
